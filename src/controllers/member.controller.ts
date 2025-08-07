0ea362834c17a5e4213be877ba430a22290d36bb
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-08-07 22:44:32 +0900

    feat: develop verify authenticated user credentials

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -5,7 +5,7 @@ import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import AuthService from "../models/Auth.service";
-import Errors, { HttpCode } from "../libs/types/Errors";
+import Errors, { HttpCode, Message } from "../libs/types/Errors";
 import { AUTH_TIMER } from "../libs/types/config";
 
 const memberService = new MemberService();
@@ -49,6 +49,21 @@ memberController.login = async (req: Request, res: Response) => {
     res.status(HttpCode.OK).json({ member: result, accessToken: token });
   } catch (err) {
     console.log("Error processLogin:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
+memberController.verifyAuth = async (req: Request, res: Response) => {
+  try {
+    let member = null;
+    const token = req.cookies["accessToken"];
+    if (token) member = await authService.checkAuth(token);
+    if (!member)
+      throw new Errors(HttpCode.UNAUTHORIZED, Message.NOT_AUTHENTICATED);
+    res.status(HttpCode.OK).json({ member: member });
+  } catch (err) {
+    console.log("Error verifyAuth:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standard.code).json(Errors.standard);
   }
--- src/router.ts
+++ src/router.ts
@@ -3,8 +3,9 @@ const router = express.Router();
 import memberController from "./controllers/member.controller";
 
 /** Member **/
-router.post("/login", memberController.login); // call
-router.post("/signup", memberController.signup); // call
+router.post("/member/login", memberController.login); // call
+router.post("/member/signup", memberController.signup); // call
+router.get("/member/detail", memberController.verifyAuth);
 
 /** Product **/
 
