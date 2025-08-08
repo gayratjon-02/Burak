448f230e05d72a78aef5c4975d974a832f58d396
gayratjon-02 <s-202386005@sillain.ac.kr>
2025-08-08 20:58:32 +0900

    fix: modify getMemberDetail business logic

--- src/router.ts
+++ src/router.ts
@@ -5,8 +5,16 @@ import memberController from "./controllers/member.controller";
 /** Member **/
 router.post("/member/login", memberController.login); // call
 router.post("/member/signup", memberController.signup); // call
-router.post("/member/logout", memberController.verifyAuth, memberController.logout);
-router.get("/member/detail", memberController.verifyAuth);
+router.post(
+  "/member/logout",
+  memberController.verifyAuth,
+  memberController.logout
+);
+router.get(
+  "/member/detail",
+  memberController.verifyAuth,
+  memberController.getMemberDetail
+);
 
 /** Product **/
 
